de2b2dda6195c4ac6708093b6db2957bb9a3989c
RTSolutionsLarry <larry.randolph@randolphtechnologysolutions.com>
2025-08-26 22:53:44 -0700

    Set up grid

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const sketchContainer = document.querySelector('.sketchContainer');
 
 const gridSetup = (size) => {
-    for (let x = 0; i < size; i++) {
-        const box = document.createElement('div')
-        box.classList.add('box');
-        
-        sketchContainer.appendChild(box);
+
+
+
+    for (let x = 0; x < size; x++) {
+        const boxRow = document.createElement('div');
+        boxRow.classList.add('boxRow');
+        sketchContainer.appendChild(boxRow)
+
+        for (let i = 0; i < size; i++) {
+            const box = document.createElement('div');
+            box.classList.add('box');
+            boxRow.appendChild(box);
+        }
     }
+
 }
 
 gridSetup(8);
